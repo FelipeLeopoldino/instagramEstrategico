70ef88ed5fb784aa603abdf872bcf723b33c324d
FelipeLEopoldino <felipeleopoldino01@icloud.com>
2023-02-04 15:04:07 -0300

    acerto gramatica

--- src/components/DataEncontro/index.jsx
+++ src/components/DataEncontro/index.jsx
@@ -8,11 +8,11 @@ export default function DataEncontro(){
       <p>
         1°,7,15 de Março ás 20h com no
         <b> mínimo 2 horas de duração,</b> separe esse tempo para
-        <b>revolucionar</b> o seu modo de enxergar a internet.
+        <b> revolucionar</b> o seu modo de enxergar a internet.
       </p>
       <br />
       <p>
-        Na Masterclass Instagram Estratégico[MIE] - Fevereiro será um mês de
+        Na Masterclass Instagram Estratégico[MIE] - Março será um mês de
         muito <b>desenvolvimento e aprendizado</b> exclusivo para você.
       </p>
     </div>
